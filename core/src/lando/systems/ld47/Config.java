5aed0700e39461c269549a851b77bb2e57aa243e
Doug Graham <dsgraham@gmail.com>
2020-09-30 18:36:56 -0500

    Add sprite packer and jenkins file

---
+++ core/src/lando/systems/ld47/Config.java
@@ -0,0 +1,13 @@
+package lando.systems.ld47;
+
+public class Config {
+    public static String title = "Ludum Dare 46";
+    public static int windowWidth = 1280;
+    public static int windowHeight = 720;
+    public static boolean resizable = true;
+    public static boolean fullscreen = false;
+    public static boolean vsync = true;
+    public static boolean debug = false;
+    public static boolean shaderDebug = false;
+    public static boolean showLaunchScreen = false;
+}
--- desktop/src/lando/systems/ld47/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld47/desktop/DesktopLauncher.java
@@ -2,11 +2,19 @@ package lando.systems.ld47.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld47.Config;
 import lando.systems.ld47.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = Config.title;
+		config.width = Config.windowWidth;
+		config.height = Config.windowHeight;
+		config.resizable = Config.resizable;
+		config.fullscreen = Config.fullscreen;
+		config.vSyncEnabled = Config.vsync;
+		config.forceExit = false;
 		new LwjglApplication(new Game(), config);
 	}
 }
