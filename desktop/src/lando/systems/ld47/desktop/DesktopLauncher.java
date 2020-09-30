aeee1ed7bbbd5be29c49f5d3bca232f9d08482b1
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2020-09-30 17:12:10 -0500

    Initial commit

---
+++ desktop/src/lando/systems/ld47/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.ld47.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld47.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
