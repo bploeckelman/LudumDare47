d0062ef1a81425b0df380fdecd85140dd6a1b9e4
Doug Graham <dsgraham@gmail.com>
2020-09-30 19:58:52 -0500

    update to use Java 8 add Tween Engine Resize HTML build

--- core/src/lando/systems/ld47/Game.java
+++ core/src/lando/systems/ld47/Game.java
@@ -1,17 +1,40 @@
 package lando.systems.ld47;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import lando.systems.ld47.accessors.*;
 
 public class Game extends ApplicationAdapter {
+	public TweenManager tween;
+
+
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+
+		if (tween == null) {
+			tween = new TweenManager();
+			Tween.setWaypointsLimit(4);
+			Tween.setCombinedAttributesLimit(4);
+			Tween.registerAccessor(Color.class, new ColorAccessor());
+			Tween.registerAccessor(Rectangle.class, new RectangleAccessor());
+			Tween.registerAccessor(Vector2.class, new Vector2Accessor());
+			Tween.registerAccessor(Vector3.class, new Vector3Accessor());
+			Tween.registerAccessor(OrthographicCamera.class, new CameraAccessor());
+		}
+
 		batch = new SpriteBatch();
 		img = new Texture("images/badlogic.jpg");
 	}
