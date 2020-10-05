d84303c7329f8575b0f66b8993eeb820e8a5cafe
Brian <brossman@kiio.com>
2020-10-05 16:37:56 -0500

    title car

---
+++ core/src/lando/systems/ld47/entities/TitleCar.java
@@ -0,0 +1,117 @@
+package lando.systems.ld47.entities;
+
+import aurelienribon.tweenengine.Tween;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import lando.systems.ld47.Game;
+
+public class TitleCar {
+
+    public enum State {
+        idle, moving, stun, ramming
+    }
+
+    public enum Direction {
+        right, left
+    }
+
+    public enum AnimDirection {
+        level, up, down
+    }
+
+    public Game game;
+
+    private float animationTime = 0;
+    private float idleTime = 0;
+    private float offsetY = 0; // for idle hovering
+
+    public final Vector2 position = new Vector2();
+    public Vector2 size = new Vector2();
+    public Direction direction = Direction.right;
+
+    private State state = State.idle;
+
+    private Animation<TextureRegion> animation = null;
+
+    public AnimDirection animState = AnimDirection.level;
+
+    public final float carScale = 0.5f;
+
+    public TitleCar(Game game) {
+        this.game = game;
+        TextureRegion texture = game.assets.car.getKeyFrame(0);
+        size.set(new Vector2(texture.getRegionWidth() * carScale, texture.getRegionHeight() * carScale));
+
+    }
+
+    public void setState(State state) {
+        this.state = state;
+        switch (state) {
+            case moving:
+                lastX = position.x;
+                lastY = position.y;
+                animation = game.assets.car;
+                break;
+            default:
+                animation = game.assets.car;
+        }
+        animationTime = 0;
+    }
+
+    private float lastX = -1;
+    private float lastY = -1;
+    public void update(float dt) {
+        animationTime += dt;
+
+        if (state == State.idle) {
+            idleTime += dt;
+            offsetY = MathUtils.sin(idleTime * 2f) * 5;
+        } else if (state == State.moving) {
+            if (position.x != lastX) {
+                direction = (position.x > lastX) ? Direction.right : Direction.left;
+            }
+            setAnimState();
+            lastY = position.y;
+            lastX = position.x;
+
+            switch (animState) {
+                case up:
+                    animation = game.assets.carUp;
+                    break;
+                case down:
+                    animation = game.assets.carDown;
+                    break;
+                default:
+                    animation = game.assets.car;
+            }
+        }
+    }
+
+    private void setAnimState() {
+        float dy = position.y - lastY;
+        if (Math.abs(dy) < 2f) {
+            animState = AnimDirection.level;
+        } else {
+            animState = (dy) > 0 ? AnimDirection.up : AnimDirection.down;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        if (animation != null) {
+
+            batch.setColor(Color.WHITE);
+
+            TextureRegion texture = animation.getKeyFrame(animationTime);
+            batch.draw(texture, position.x, position.y + offsetY, size.x / 2, size.y / 2, size.x, size.y,
+                    (direction == Direction.right) ? -1 : 1, 1, 0);
+        }
+    }
+
+    public float getWidth() {
+        return (animation != null) ? animation.getKeyFrame(0).getRegionWidth() : 0;
+    }
+}
--- core/src/lando/systems/ld47/screens/TitleScreen.java
+++ core/src/lando/systems/ld47/screens/TitleScreen.java
@@ -4,19 +4,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import lando.systems.ld47.Audio;
 import lando.systems.ld47.Game;
+import lando.systems.ld47.entities.TitleCar;
 
 public class TitleScreen extends BaseScreen {
+
+    private TitleCar car;
+
     public TitleScreen(Game game) {
         super(game);
         playMusic(Audio.Musics.intro_track);
         playerInput.recheckController();
+
+        car = new TitleCar(game);
+        car.position.set(500, 250);
+        car.setState(TitleCar.State.idle);
     }
 
     @Override
     public void update(float dt) {
         super.update(dt);
+
+        car.update(dt);
 
         if (Gdx.input.justTouched()
          || Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)
@@ -32,6 +43,8 @@ public class TitleScreen extends BaseScreen {
 
         batch.setColor(Color.WHITE);
         batch.draw(assets.titleImage, 0,0, hudCamera.viewportWidth, hudCamera.viewportHeight);
+
+        car.render(batch);
 
         batch.end();
     }
